7af87c806f39b4d4fa6659c147c5ebcfe5089785
1926khushboo <100568922+1926khushboo@users.noreply.github.com>
2024-03-09 06:26:39 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144356;
+let accountEmail="khushbookumari1403@gmail.com";
+var accountPassword = "1234";
+accountCity="patna" //also a way to declare a variable but not a good practice.
+let accountState; // it will give a value undefined untill u assign a value to it like above varaibles.
+//accountId=1234 // can't declare a contant varibale twice in the code.
+
+/*
+prefer not to use var
+because of issues in block scope and functional scope.
+*/
+
+accountEmail="avc@gmail.com";
+accountPassword="2121";
+accountCity="rajasthan";
+console.log(accountId);
