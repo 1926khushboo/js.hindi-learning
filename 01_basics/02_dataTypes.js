bb119e6e5a01b3485ccb86321f70cfea7ecea051
1926khushboo <100568922+1926khushboo@users.noreply.github.com>
2024-03-09 06:44:34 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"
+console.log(3+3);
+console.log("khushboo")
+
+/*
+//data types: primitive
+
+number = 2^53 (range)
+bigint = used for bigger number like used in making websites like stocks,etc,.
+String = " "
+boolean = true/false
+null = standalone value and at the same time it's a datatype also. It is an object.
+undefined = means the value is not assigned.
+symbol = unique
+*/
+
+//object
+console.log(typeof "khushboo");
